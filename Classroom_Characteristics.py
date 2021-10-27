82786995f241b2538cc49a93a7e9a4e2b8ad96a1
Nuno Dias <azoresnuno@hotmail.com>
2021-10-27 14:58:42 +0100

    update_27/10/2021

---
+++ Classroom_Characteristics.py
@@ -0,0 +1,8 @@
+class Classroom_Characteristics:
+
+    def __init__(self, name, description, n_rooms, available_class_management, available_classroom_request):
+        self.name = name
+        self.description = description
+        self.n_rooms = n_rooms
+        self.available_class_management = available_class_management
+        self.available_classroom_request = available_classroom_request
