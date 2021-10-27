852389ba908a0752ac9b3440c9a2a3b4ab38687e
Nuno Dias <azoresnuno@hotmail.com>
2021-10-27 13:38:21 +0100

    project_base

---
+++ Manipulate_Documents.py
@@ -0,0 +1,13 @@
+import os
+
+input_path = "Input_Documents"
+output_path = "Output_Documents"
+ext = [".csv", ".xml", ".json", ".bd"]
+
+for root, dirs, files in os.walk(input_path):
+    for file in files:
+        if file.endswith(tuple(ext)):
+            file_to_open = os.path.join(input_path, file)
+            f = open(file_to_open, 'r')
+            # meter nas classes necessárias
+            f.close()
