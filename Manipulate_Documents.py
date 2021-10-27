070fafcecb4419789e56a15d5735631f9b203b69
Nuno Dias <azoresnuno@hotmail.com>
2021-10-27 15:11:43 +0100

    updt

--- Manipulate_Documents.py
+++ Manipulate_Documents.py
@@ -1,4 +1,7 @@
+import csv
 import os
+
+from Classroom import Classroom
 
 
 class Manipulate_Documents:
@@ -20,12 +23,21 @@ class Manipulate_Documents:
                     f.close()
 
     def import_classrooms(self):
+        classroom_list = []
         for root, dirs, files in os.walk(self.input_classrooms):
             for file in files:
                 if file.endswith(tuple(self.ext)):
                     file_to_open = os.path.join(self.input_classrooms, file)
                     f = open(file_to_open, 'r')
-                    print(f.name)
+                    csvreader = csv.reader(f)
+                    header = next(csvreader)
+                    if f.name == 'Input_Classrooms\Características.csv':
+                        print(f.name)
+                        for row in csvreader:
+                            classroom = Classroom(row[0], row[1], row[2], row[3], [])
+                            # classroom_characteristics
+                            classroom_list.append(row)
+                            # print(classroom_list)
                     # meter nas classes necessárias
                     f.close()
 
