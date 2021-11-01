fab5b662c6fac7d36e8aed192667012246b68347
Nuno Dias <azoresnuno@hotmail.com>
2021-11-01 17:25:43 +0000

    updt

---
+++ Classroom.py
@@ -0,0 +1,6 @@
+class Classroom:
+
+    def __init__(self, building, name, normal_capacity, exam_capacity, characteristics):
+        self.building = building
+        self.name = name
+        self.characteristics = characteristics
