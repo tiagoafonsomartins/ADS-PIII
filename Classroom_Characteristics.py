1bf57a7dadc4425ebae0f31ea5394cd32e4513cf
Nuno Dias <azoresnuno@hotmail.com>
2021-11-01 18:05:41 +0000

    useless classes deleted

--- Classroom_Characteristics.py
+++
@@ -1,8 +0,0 @@
-class Classroom_Characteristics:
-
-    def __init__(self, name, description, n_rooms, available_class_management, available_classroom_request):
-        self.name = name
-        self.description = description
-        self.n_rooms = n_rooms
-        self.available_class_management = available_class_management
-        self.available_classroom_request = available_classroom_request
