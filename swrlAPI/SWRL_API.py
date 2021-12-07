cf87185ff98395a91d8a997b2f793c4b14333bf8
Nuno Dias <azoresnuno@hotmail.com>
2021-12-07 15:43:22 +0000

    Update SWRL_API.py

--- swrlAPI/SWRL_API.py
+++ swrlAPI/SWRL_API.py
@@ -2,6 +2,7 @@ import subprocess
 query_result_temp = subprocess.check_output(['java', '-jar', 'SWRLAPIExample.jar', "3"])
 print(query_result_temp)
 query_result = []
+
 for i in query_result_temp.decode("utf-8").split("Result:")[1].split("\n")[1:-1]:
     aux_clean_alg = i.split("\r")[0]
     clean_alg = aux_clean_alg.split(":")[1]
