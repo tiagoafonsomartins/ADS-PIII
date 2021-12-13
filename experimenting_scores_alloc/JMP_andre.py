7066f004516a8f921810a086a038f7250ff129d0
SaltedC0okie <56958324+SaltedC0okie@users.noreply.github.com>
2021-12-13 21:36:58 +0000

    SEi lá J à

--- experimenting_scores_alloc/JMP_andre.py
+++ experimenting_scores_alloc/JMP_andre.py
@@ -8,48 +8,44 @@ from typing import List
 import numpy as np
 import random
 
+from experimenting_scores_alloc.AndreAllocProblem import AndreAllocProblem
 from file_manager.Manipulate_Documents import Manipulate_Documents
 from jmetalpy.TimeTablingProblem import TimeTablingProblem
 from metrics.Metric import *
 
 
-class JMP:
+class JMP_andre:
 
     def run_algorithm(self, alg_list: list, lessons: list, classrooms: list, metrics: list) -> list:
         for a in alg_list:
             try:
-                alg = getattr(JMP, a)
+                alg = getattr(JMP_andre, a)
                 break
             except:
-                alg = getattr(JMP, "nsgaii")
+                alg = getattr(JMP_andre, "floatnsgaii")
 
-        problem = TimeTablingProblem(lessons, classrooms, metrics)
-        # problem = Problem(lessons, classrooms, metrics)
+        #problem = TimeTablingProblem(lessons, classrooms, metrics)
+        problem = AndreAllocProblem(lessons, classrooms, metrics)
 
         algorithm = alg(problem)
 
-        print(len(lessons), " Lessons: ", lessons)
-        print(len(classrooms), "Classrooms: ", classrooms)
         start = time.time()
         algorithm.run()
         elapsed_time = time.time() - start
+        print("Elapsed time: ", elapsed_time)
 
-        # print("Elapsed time: ", elapsed_time)
-
-
-        print("hi")
         solutions = algorithm.get_result()
         front = get_non_dominated_solutions(solutions)
 
-        # for solution in front:
-        # print("solution: ", solution.objectives)
+        for solution in front:
+            print("solution: ", solution.objectives, " ,  ", solution.variables)
 
-        result = self.get_best_result(front, metrics)
+        #result = self.get_best_result(front, metrics)
 
-        new_classrooms = [classroom for classroom in result.variables]
-        new_schedule = [(lessons[i], classrooms[new_classrooms[i]]) for i in range(len(lessons))]
+        #new_classrooms = [classroom for classroom in result.variables]
+        #new_schedule = [(lessons[i], new_classrooms[i]) for i in range(len(lessons))]
 
-        return new_schedule
+        #return new_schedule
 
     # The weights in each metric are in a range of 0 to 1
     def get_best_result(self, front, metrics):
@@ -59,10 +55,15 @@ class JMP:
         # Choosing the percentage said in the weight within the range (as in from min to max) of each objective
         objectives_scores = []
         for i in range(len(metrics)):
-            objective = [result.objectives[i] for result in front]
+            objective      = [result.objectives[i] for result in front]
             objective_lims = (min(objective), max(objective))
-            objectives_scores.append(
-                (objective_lims[1] - objective_lims[0]) * (1 - metrics[i].weight) + objective_lims[0])
+            objectives_scores.append((objective_lims[1] - objective_lims[0]) * (1-metrics[i].weight) + objective_lims[0])
+
+        string = "Alg scores: ["
+        for score in objectives_scores:
+            string += str(score) + ", "
+        string = string[:-2] + "]"
+        print(string)
 
         # Choosing which result is closer on average of
         chosen_result = None
@@ -75,28 +76,27 @@ class JMP:
             if new_prox < chosen_proximity:
                 chosen_result = result
                 chosen_proximity = new_prox
-
+        print(chosen_result.objectives)
         return chosen_result
 
     # The weights are in a range of 0 to 1
-    def get_best_result_static(self, front, roomless_weight, overbooking_weight, underbooking_weight,
-                               badclassroom_weight):
+    def get_best_result_static(self, front, roomless_weight, overbooking_weight, underbooking_weight, badclassroom_weight):
 
         # Making lists of each objective's value
-        roomlesses = [result.objective[0] for result in front]
-        overbookings = [result.objective[1] for result in front]
+        roomlesses    = [result.objective[0] for result in front]
+        overbookings  = [result.objective[1] for result in front]
         underbookings = [result.objective[2] for result in front]
         badclassrooms = [result.objective[3] for result in front]
 
         # Getting the max and min of each objective
-        roomless_lims = (min(roomlesses), max(roomlesses))
-        overbooking_lims = (min(overbookings), max(overbookings))
+        roomless_lims     = (min(roomlesses), max(roomlesses))
+        overbooking_lims  = (min(overbookings), max(overbookings))
         underbooking_lims = (min(underbookings), max(underbookings))
         badclassroom_lims = (min(badclassrooms), max(badclassrooms))
 
         # Choosing the percentage said in the weight within the range of each objective
-        roomless_score = (roomless_lims[1] - roomless_lims[0]) * roomless_weight + roomless_lims[0]
-        overbooking_score = (overbooking_lims[1] - overbooking_lims[0]) * overbooking_weight + overbooking_lims[0]
+        roomless_score     = (roomless_lims[1] - roomless_lims[0]) * roomless_weight + roomless_lims[0]
+        overbooking_score  = (overbooking_lims[1] - overbooking_lims[0]) * overbooking_weight + overbooking_lims[0]
         underbooking_score = (underbooking_lims[1] - underbooking_lims[0]) * underbooking_weight + underbooking_lims[0]
         badclassroom_score = (badclassroom_lims[1] - badclassroom_lims[0]) * badclassroom_weight + badclassroom_lims[0]
 
@@ -105,9 +105,9 @@ class JMP:
         chosen_proximity = float('inf')
         for result in front:
             new_prox = self.distance(result.objective[0], roomless_score) + \
-                       self.distance(result.objective[1], overbooking_score) + \
-                       self.distance(result.objective[2], underbooking_score) + \
-                       self.distance(result.objective[3], badclassroom_score)
+                              self.distance(result.objective[1], overbooking_score) + \
+                              self.distance(result.objective[2], underbooking_score) + \
+                              self.distance(result.objective[3], badclassroom_score)
             if new_prox < chosen_proximity:
                 chosen_result = result
                 chosen_proximity = new_prox
@@ -127,6 +127,16 @@ class JMP:
             termination_criterion=StoppingByEvaluations(max_evaluations=10000)
         )
 
+    def floatnsgaii(problem):
+        return NSGAII(
+            problem=problem,
+            population_size=100,
+            offspring_population_size=100,
+            mutation=PolynomialMutation(probability=0.5),  # (probability=1.0 / problem.number_of_variables),
+            crossover=SBXCrossover(probability=0.5),
+            termination_criterion=StoppingByEvaluations(max_evaluations=1000) # TODO
+        )
+
     def nsgaiii(problem):
         return NSGAIII(
             problem=problem,
@@ -138,28 +148,3 @@ class JMP:
         )
 
 
-#    def insgaii(problem):
-#        return NSGAII(
-#            problem=problem,
-#            population_size=500,
-#            offspring_population_size=500,
-#            mutation=IntegerPolynomialMutation(probability=0.4),  # (probability=1.0 / problem.number_of_variables),
-#            crossover=DrenasCrossover(),
-#            termination_criterion=StoppingByEvaluations(max_evaluations=1000)
-#        )
-
-
-def testar():
-    md = Manipulate_Documents("../input_documents", "../output_documents", "../input_classrooms")
-    classrooms = md.import_classrooms()
-    gangs, l = md.import_schedule_documents("Exemplo_de_horario_segundo_semestre.csv", False)
-    metrics = [RoomlessLessons(), Overbooking(), Underbooking(), BadClassroom()]
-    # metrics = [Overbooking()]
-    lessons = [le[0] for le in l][5000:5040]
-
-    result = JMP().run_algorithm("nsgaii", lessons, classrooms, metrics)
-    print(result)
-
-
-if __name__ == "__main__":
-    testar()
--- experiments.py
+++ experiments.py
@@ -168,8 +168,203 @@ class Experiments:
         print("Elapsed time: ", elapsed_time, "\n")
 
 
-"""
+    def test6(self):
+        lesson = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "3:00:00", "10:00:00", "4/23/2005",
+                        "Good, Not stinky, Very good")
+        print(sys.getsizeof(lesson.requested_characteristics))
+        print(sys.getsizeof(Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "10:00:00", "10:30:00", "4/23/2005",
+                                   "Good, Not stinky, Very good")))
+        print(sys.getsizeof(""))
+
+
+    def get_earliest(self):
+        pass
+
+
+    def test7(self):
+        md = Manipulate_Documents()
+        results = md.import_schedule_documents(True)
+        lessons = results[1]
+
+        earliest = "23:59:59"
+        latest = "00:00:00"
+        for lesson in lessons:
+            if lesson[0].start == " " or lesson[0].start == "": continue
+            if lesson[0].end == " " or lesson[0].end == "": continue
+            new_start = lesson[0].start
+            new_end = lesson[0].end
+            if len(lesson[0].start) < 8: new_start = "0" + new_start
+            if len(lesson[0].end) < 8: new_end = "0" + new_end
+            # print(lesson.start)
+            # print(lesson.end, "\n")
+            print(new_start, "<", earliest)
+            if new_start < earliest:
+                earliest = new_start
+                print(True)
+            if new_end > latest:
+                latest = new_end
+
+        print("earliest: ", earliest)
+        print("latest: ", latest)
+
+
+    def test8(self):
+        string1 = "a"
+        string2 = "b"
+        print(string1 < string2)
+
+
+    def test9(self):
+        a, b = get_tuplo()
+        print(a)
+        print(b)
+
+
+    def test10(self):
+        ngosta = getattr(Metric, 'OverBooking')
+        atr = ngosta("fdsa", 2)
+        atr.testing("fkljdsa")
+        print(atr.value)
+        print(atr.name)
+
+
+    def test11(self):
+        metrics = [Metric.Overbooking(), Metric.Gaps()]
+
+        for metric in [m for m in metrics if m.m_type == "lesson"]:
+            metric.calculate()
+
+
+    def test12(self):
+        lesson1 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "3:00:00", "10:00:00", "4/23/2005",
+                         "Good, Not stinky, Very good")
+        lesson2 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "11:00:00", "12:00:00", "4/23/2005",
+                         "Good, Not stinky, Very good")
+        lesson3 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "13:00:00", "15:00:00", "4/23/2005",
+                         "Good, Not stinky, Very good")
+        classroom1 = Classroom('Edifício 69', 'Auditório 420', 50, 25, ["cenas"])
+        classroom2 = Classroom('Edifício 2', 'B203', 150, 125, ["cenas", "mais cenas", "ainda mais cenas"])
+        lesson1.add_classroom(classroom1)
+        lesson2.add_classroom(classroom2)
+        lesson3.add_classroom(classroom2)
+
+        gang = Gang("best", "lei", lesson1)
+        gang.add_lesson(lesson2)
+        gang.add_lesson(lesson3)
+
+        g = Gaps()
+        g.calculate(gang)
+        # m = Movements()
+        # m.calculate(gang)
+        u_r = UsedRooms()
+        u_r.calculate(lesson1, classroom1)
+        u_r.calculate(lesson2, classroom1)
+        u_r.calculate(lesson3, classroom1)
+
+        print(g.value)
+        # print(m.value)
+        print(u_r.value)
+
+
+    def test13(self):
+        dix = {}
+        for i in range(10000000):
+            dix[str(i)] = i
+
+        start = time.time()
+        if '99999999' in dix.keys():
+            print("caralho")
+        else:
+            print('what')
+        elapsed_time = time.time() - start
+
+        print("Elapsed time: ", elapsed_time)
+
+
+    class TestYa:
+        def __init__(self, i):
+            self.i = i
+
+        def __repr__(self):
+            return str(self.i)
+
+
+    def test14(self):
+        l = []
+        for i in range(1000):
+            l.append((self.TestYa(i), self.TestYa(i)))
+        for i, ip1 in l:
+            ip1.i = ip1.i + 1
+
+        print(l)
+
+
+    def test15(self):
+        cena = None
+        if cena and cena + 1 == 2:
+            print("hello")
+        else:
+            print("Não hello")
+
+
+    def test16(self):
+        md = Manipulate_Documents()
+        classrooms = md.import_classrooms()
+        gangs, schedule = md.import_schedule_documents(False)
+        days = set()
+        for i, tuple in enumerate(schedule):
+            # if tuple[0].start.split(":")[1] != "00" and tuple[0].start.split(":")[1] != "30":
+            # print("Here: ", i)
+            # print(tuple[0].end)
+            if tuple[0].day not in days:
+                days.add(tuple[0].day)
+                print(tuple[0].day)
+        print(len(days))
+
+
+    def test17(self):
+        l = [1, 2, 3]
+        print(l[:2])
+        print(l[2:])
+
+
+    def test18(self):
+        md = Manipulate_Documents()
+        classrooms = md.import_classrooms()
+        gangs, schedule, date = md.import_schedule_documents(False)
+        print(date)
+
+        max = 0
+        min = 20
+        for c in classrooms:
+            if len(c.get_characteristics()) > max: max = len(c.get_characteristics())
+            if len(c.get_characteristics()) > min: min = len(c.get_characteristics())
+
+        print(max)
+        print(min)
+        # a = Allocator(classrooms, schedule, gangs)
+        # a.get_index_of_block()
+
+
+    def test19(self):
+        l = {}
+        l["a"] = 1
+        l["b"] = 2
+
+        if "c" in l.keys():
+            print("hello")
+        else:
+            print("Adeus")
+
+
+    def test20(self):
         # andre_allocation
+        md = Manipulate_Documents()
+        classrooms = md.import_classrooms()
+        gangs, schedule = md.import_schedule_documents("Exemplo_de_horario_primeiro_semestre.csv", False)
+
+        a_simple = Allocator(classrooms, schedule, gangs)
+
         a_simple.remove_all_allocations()
 
         start = time.time()
@@ -205,214 +400,25 @@ class Experiments:
 
         print("\n\nandre_algorithm:\n")
         print("Roomless Lessons Percentage:", round(room_metric.get_percentage(), 2) * 100, "%")
-        print("Overbooking Percentage:", round(overbooking_metric.get_total_metric_value(), 2) * 100, "%")
-        print("Underbooking Percentage:", round(underbooking_metric.get_total_metric_value(), 2) * 100, "%")
+        print("Overbooking Percentage:", round(overbooking_metric.get_percentage(), 2) * 100, "%")
+        print("Underbooking Percentage:", round(underbooking_metric.get_percentage(), 2) * 100, "%")
         print("Bad Classroom Percentage:", round(bad_classroom_metric.get_percentage(), 2) * 100, "%")
-
 
         print("Elapsed time: ", elapsed_time)
         print("Elapsed time on metricas: ", elapsed_time_metricas)
         print("Elapsed time on convert: ", elapsed_time_convert)
         print("Tamanho do lessons30: ", tamanho)
 
+    def test21(self):
+        l = {}
+        l["a"] = 1
+        l["b"] = 2
+        l["c"] = 69
 
-        #md.export_schedule_lessons30(andre_schedule, "Teste_andre")
-        #md.export_schedule(andre_schedule, "outputMens")
-"""
+        for block, half_hour in l.items():
+            l[block] = 3
 
-
-def test6(self):
-    lesson = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "3:00:00", "10:00:00", "4/23/2005",
-                    "Good, Not stinky, Very good")
-    print(sys.getsizeof(lesson.requested_characteristics))
-    print(sys.getsizeof(Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "10:00:00", "10:30:00", "4/23/2005",
-                               "Good, Not stinky, Very good")))
-    print(sys.getsizeof(""))
-
-
-def get_earliest(self):
-    pass
-
-
-def test7(self):
-    md = Manipulate_Documents()
-    results = md.import_schedule_documents(True)
-    lessons = results[1]
-
-    earliest = "23:59:59"
-    latest = "00:00:00"
-    for lesson in lessons:
-        if lesson[0].start == " " or lesson[0].start == "": continue
-        if lesson[0].end == " " or lesson[0].end == "": continue
-        new_start = lesson[0].start
-        new_end = lesson[0].end
-        if len(lesson[0].start) < 8: new_start = "0" + new_start
-        if len(lesson[0].end) < 8: new_end = "0" + new_end
-        # print(lesson.start)
-        # print(lesson.end, "\n")
-        print(new_start, "<", earliest)
-        if new_start < earliest:
-            earliest = new_start
-            print(True)
-        if new_end > latest:
-            latest = new_end
-
-    print("earliest: ", earliest)
-    print("latest: ", latest)
-
-
-def test8(self):
-    string1 = "a"
-    string2 = "b"
-    print(string1 < string2)
-
-
-def test9(self):
-    a, b = get_tuplo()
-    print(a)
-    print(b)
-
-
-def test10(self):
-    ngosta = getattr(Metric, 'OverBooking')
-    atr = ngosta("fdsa", 2)
-    atr.testing("fkljdsa")
-    print(atr.value)
-    print(atr.name)
-
-
-def test11(self):
-    metrics = [Metric.Overbooking(), Metric.Gaps()]
-
-    for metric in [m for m in metrics if m.m_type == "lesson"]:
-        metric.calculate()
-
-
-def test12(self):
-    lesson1 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "3:00:00", "10:00:00", "4/23/2005",
-                     "Good, Not stinky, Very good")
-    lesson2 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "11:00:00", "12:00:00", "4/23/2005",
-                     "Good, Not stinky, Very good")
-    lesson3 = Lesson("MEI", "ADS", "69420blz", "t-69", 420, "Sex", "13:00:00", "15:00:00", "4/23/2005",
-                     "Good, Not stinky, Very good")
-    classroom1 = Classroom('Edifício 69', 'Auditório 420', 50, 25, ["cenas"])
-    classroom2 = Classroom('Edifício 2', 'B203', 150, 125, ["cenas", "mais cenas", "ainda mais cenas"])
-    lesson1.add_classroom(classroom1)
-    lesson2.add_classroom(classroom2)
-    lesson3.add_classroom(classroom2)
-
-    gang = Gang("best", "lei", lesson1)
-    gang.add_lesson(lesson2)
-    gang.add_lesson(lesson3)
-
-    g = Gaps()
-    g.calculate(gang)
-    # m = Movements()
-    # m.calculate(gang)
-    u_r = UsedRooms()
-    u_r.calculate(lesson1, classroom1)
-    u_r.calculate(lesson2, classroom1)
-    u_r.calculate(lesson3, classroom1)
-
-    print(g.value)
-    # print(m.value)
-    print(u_r.value)
-
-
-def test13(self):
-    dix = {}
-    for i in range(10000000):
-        dix[str(i)] = i
-
-    start = time.time()
-    if '99999999' in dix.keys():
-        print("caralho")
-    else:
-        print('what')
-    elapsed_time = time.time() - start
-
-    print("Elapsed time: ", elapsed_time)
-
-
-class TestYa:
-    def __init__(self, i):
-        self.i = i
-
-    def __repr__(self):
-        return str(self.i)
-
-
-def test14(self):
-    l = []
-    for i in range(1000):
-        l.append((self.TestYa(i), self.TestYa(i)))
-    for i, ip1 in l:
-        ip1.i = ip1.i + 1
-
-    print(l)
-
-
-def test15(self):
-    cena = None
-    if cena and cena + 1 == 2:
-        print("hello")
-    else:
-        print("Não hello")
-
-
-def test16(self):
-    md = Manipulate_Documents()
-    classrooms = md.import_classrooms()
-    gangs, schedule = md.import_schedule_documents(False)
-    days = set()
-    for i, tuple in enumerate(schedule):
-        # if tuple[0].start.split(":")[1] != "00" and tuple[0].start.split(":")[1] != "30":
-        # print("Here: ", i)
-        # print(tuple[0].end)
-        if tuple[0].day not in days:
-            days.add(tuple[0].day)
-            print(tuple[0].day)
-    print(len(days))
-
-
-def test17(self):
-    l = [1, 2, 3]
-    print(l[:2])
-    print(l[2:])
-
-
-def test18(self):
-    md = Manipulate_Documents()
-    classrooms = md.import_classrooms()
-    gangs, schedule, date = md.import_schedule_documents(False)
-    print(date)
-
-    max = 0
-    min = 20
-    for c in classrooms:
-        if len(c.get_characteristics()) > max: max = len(c.get_characteristics())
-        if len(c.get_characteristics()) > min: min = len(c.get_characteristics())
-
-    print(max)
-    print(min)
-    # a = Allocator(classrooms, schedule, gangs)
-    # a.get_index_of_block()
-
-
-def test19(self):
-    l = {}
-    l["a"] = 1
-    l["b"] = 2
-
-    if "c" in l.keys():
-        print("hello")
-    else:
-        print("Adeus")
-
-
-def test20(self):
-	print(random.uniform(0.1, 0.657))
-
+        print(l)
 
 
 def get_tuplo():
