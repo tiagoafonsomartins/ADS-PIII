1bfd12b3135e774c23a4db8f2f09589fe37a0e05
Nuno Dias <azoresnuno@hotmail.com>
2021-12-13 15:47:12 +0000

    Update SWRL_API.py

--- swrlAPI/SWRL_API.py
+++ swrlAPI/SWRL_API.py
@@ -3,7 +3,7 @@ import subprocess
 
 def query_result():
     query_result_temp = subprocess.run(
-        ['java', '-jar', "C:\\Users\\Nuno Dias\\Documents\\GitHub\\ADS-PIII\\swrlAPI\\SWRLAPIExample.jar", '3'],
+        ['java', '-jar', "swrlAPI\\SWRLAPIExample.jar", '3'],
         universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     query_result = []
     for i in query_result_temp.stdout.split("Result:")[1].split("\n")[1:-1]:
