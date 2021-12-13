ed5b7f0482a0f819c0c9c52d9b19f293b93765e1
rhinestein <multitiagomartins@gmail.com>
2021-12-13 21:31:22 +0000

    Correct importing of file onto Manipulate_Documents

--- ads_app/views.py
+++ ads_app/views.py
@@ -11,7 +11,8 @@ from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.http import HttpResponse, Http404
 import json
-
+import io
+from lesson.Lesson import *
 
 def index(request):
     return render(request, 'index.html')
@@ -20,6 +21,41 @@ def index(request):
 def results(request):
     if request.method == 'POST' and request.FILES['filename']:
         myFile = request.FILES['filename']
+        #f = myFile.read().decode('utf-8')
+        myFile.seek(0)
+        #csvreader = csv.DictReader(io.StringIO(myFile.read().decode('utf-8')))
+        #print(f)
+        #dialect = csv.Sniffer().sniff(codecs.EncodedFile(myFile,"utf-8").read(1024))
+        #f.open() # seek to 0
+        #csvreader = csv.reader(codecs.EncodedFile(myFile,"utf-8"),
+        #                        dialect=dialect)
+        #f = codecs.EncodedFile(request.FILES['filename'],"utf-8")
+        csvreader = csv.reader(io.StringIO(myFile.read().decode('utf-8')))
+        lesson_list = []
+        gang_list = []
+        create_gang = True
+        #for root, dirs, files in os.walk(self.input_path):
+        #    for file in files:
+        #        if file.endswith(tuple(self.ext)):
+        #            file_to_open = os.path.join(self.input_path, file)
+        #            f = open(file_to_open, 'r', encoding="utf8")
+        next(csvreader)
+        for row in csvreader:
+            print(row)
+            lesson = Lesson(row[0], row[1], row[2], row[3], int(row[4]), row[5], row[6], row[7], row[8], row[9])
+            lesson_list.append(lesson)
+            for gang in gang_list:
+                if gang.name == lesson.gang: # TODO tratar de várias turmas para a mesma Lesson
+                    gang.add_lesson(lesson)
+                    create_gang = False
+                    break
+            if create_gang:
+                gang_list.append(Gang(lesson.gang, lesson.course, lesson))
+            else:
+                create_gang = True
+        myFile.close()
+        print (lesson_list)
+        
         fs = FileSystemStorage()
         #filename = fs.save('\tmp\Input_Documents', myFile)
         #uploaded_file_url = fs.url(filename)
